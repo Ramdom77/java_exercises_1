f056ceb1d0e3c07352dc9f80d98a661d4aca385c
Nathan De Alba <nathan.de-alba@epita.fr>
2022-02-10 12:14:24 +0100

    Added a loop

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,15 +3,23 @@ import java.util.InputMismatchException;
 
 public class Launcher {
     public static void main(String[] args) {
-        try {
-            System.out.println("Bienvenue dans ce premier projet.");
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Entrez votre commande : ");
-            String email = scanner.next("quit");
-        } catch (InputMismatchException e) {
-
-            System.out.println("Unknown command");
-        }
+        System.out.println("Bienvenue dans ce premier projet.");
+        boolean running = true;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            try {
+                System.out.println("Entrez votre commande : ");
+                String cmd = scanner.nextLine();
+                if ("quit".equals(cmd)){
+                    running = false;
+                }
+                else{
+                    System.out.println("Unknown command");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Unknown command");
+            }
+        }while(running);
     }
 }
