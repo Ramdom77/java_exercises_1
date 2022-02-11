4941371bf4c962e4c212147b37a5495becea6758
Nathan De Alba <nathan.de-alba@epita.fr>
2022-02-11 19:42:08 +0100

    Added freq, only reading path not 3 frequent words

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,5 +1,12 @@
+import java.nio.file.Files;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+
 
 public class Launcher {
     public static void main(String[] args) {
@@ -17,8 +24,20 @@ public class Launcher {
                     System.out.println("Entrez votre nombre: ");
                     String n = scanner.nextLine();
                     System.out.println(fibo(Integer.parseInt(n)));
-                } else {
-                    System.out.println("Unknown command");
+                } else if ("freq".equals(cmd)) {
+                    Scanner sc_path = new Scanner(System.in);
+                    System.out.println("Entrer le chemin du ficher : ");
+                    Path filePath = Paths.get(sc_path.nextLine());
+                    String content = null;
+                    try {
+                        content = Files.readString(filePath);
+                        content.replaceAll(",.;.:.?.!./..", " ");
+                        System.out.println(content);
+                    } catch (IOException e) {
+                        System.out.println("Unreadable file: class Launcher; " + e);
+                    }
+
+
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Unknown command");
